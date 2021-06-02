511b20410bcf510d96d660ab4e151a0101af2f68
Jonas Cirotzki <jonas.cirotzki@posteo.de>
2021-06-02 16:04:49 +0200

    smaller diff

--- sormas-backend/src/main/java/de/symeda/sormas/backend/sormastosormas/contact/ProcessedContactDataPersister.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/sormastosormas/contact/ProcessedContactDataPersister.java
@@ -104,10 +104,7 @@ public class ProcessedContactDataPersister implements ProcessedDataPersister<Pro
 		final ContactDto savedContact;
 		if (isCreate) {
 			// save person first during creation
-			handleValidationError(
-				() -> personFacade.savePerson(processedData.getPerson(), false),
-				Captions.Person,
-				contactValidationGroupName);
+			handleValidationError(() -> personFacade.savePerson(processedData.getPerson(), false), Captions.Person, contactValidationGroupName);
 			savedContact = handleValidationError(
 				() -> contactFacade.saveContact(processedData.getEntity(), true, true, false),
 				Captions.Contact,
@@ -118,10 +115,7 @@ public class ProcessedContactDataPersister implements ProcessedDataPersister<Pro
 				() -> contactFacade.saveContact(processedData.getEntity(), true, true, false),
 				Captions.Contact,
 				contactValidationGroupName);
-			handleValidationError(
-				() -> personFacade.savePerson(processedData.getPerson(), false),
-				Captions.Person,
-				contactValidationGroupName);
+			handleValidationError(() -> personFacade.savePerson(processedData.getPerson(), false), Captions.Person, contactValidationGroupName);
 		}
 
 		if (afterSaveContact != null) {
